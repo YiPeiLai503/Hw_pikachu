b574a1b7da84157710f2d778d1a723fb56163eee
YiPeiLai503 <yipeilai503@gmail.com>
2023-07-03 16:38:19 +0800

    add new file.

---
+++ pikachuClassify.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 27 20:08:43 2023
+
+@author: L
+"""
+
+import keras
+import tensorflow as tf
+import pandas as pd
---
+++ pikachu_UI.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 16:34:32 2023
+
+@author: L
+"""
+
